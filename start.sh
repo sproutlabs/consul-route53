edc79aab5b1cf6bcb895ae2ea1187ed978bf8f8a
root <root@ip-10-0-1-16.ap-southeast-2.compute.internal>
2016-12-12 10:22:13 +0000

    Arguments being passed through properly

--- start.sh
+++ start.sh
@@ -6,10 +6,8 @@ EC2_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 node subscribe.js $1 $EC2_IP $EC2_REGION
 
-# -bind=<external ip> -retry-join=<root agent ip> -bootstrap-expect=<number of server agents>
-echo "${@:2}"
-/usr/local/bin/docker-entrypoint.sh ${@:2}
+shift
+/usr/local/bin/docker-entrypoint.sh $@
 
 
 
-#docker run consul-test consul-server.glasshouse.local agent -server -dev  -ui -bind 0.0.0.0  -client 0.0.0.0  -retry-join consul-server.glasshouse.local.  -advertise $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) -recursor=10.0.0.2
